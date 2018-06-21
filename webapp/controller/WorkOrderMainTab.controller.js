844ec5465727f27753c01b404f5b0572e1909898
ASHUTOSH MASURKAR <ashutosh.masurkar@accenture.com>
2018-06-21 12:35:24 +0000

    Work Manager Phase1 Changes by Ashutosh

---
+++ webapp/controller/WorkOrderMainTab.controller.js
@@ -0,0 +1,16 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("ZWMReplacement.controller.WorkOrderMainTab", {
+
+	onInit: function() {
+
+		//	this.getOwnerComponent().getRouter().getRoute("workOrderMainTab").attachPatternMatched(this._onRouteMatched, this);
+
+		}
+
+	});
+
+});
